f395008421799a2f949d49cfe67489d366732085
Tintya <tintya@mail.ru>
2023-03-11 16:58:57 +0500

    Add Finalwork

--- FinalWork/Program.cs
+++ FinalWork/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿void NewArray(string[] arr1, string[] arr2)
+{
+    int count = 0;
+for (int i = 0; i < arr1.Length; i++)
+{
+    if(arr1[i].Length<4)
+    {
+        arr2[count] = arr1[i];
+        count++;
+    }
+}
+}
+
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+}
+
+
+string[] array = {"Russia", "1234", "2", ":-)", "hello", "new"};
+string[] finalArray = new string[array.Length];
+NewArray(array, finalArray);
+PrintArray(finalArray);
